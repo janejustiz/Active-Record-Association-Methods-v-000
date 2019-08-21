bb30b74d0820f99e7469b390edfd077ec791d7ad
Jane Justiz <janejustiz@gmail.com>
2019-08-21 13:12:03 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,8 +11,10 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
+    array = []
     self.artists do |artist|
-      artist.name
+      array << artist.name
     end
+    array
   end
 end
